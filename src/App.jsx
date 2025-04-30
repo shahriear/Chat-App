4bb232a2114f952a44ffcc1183f63302b4c0f9ae
shuvo pc <shahriearshuvo@gmail.com>
2025-04-30 12:56:02 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     createRoutesFromElements(
       <Route path="/">
         <Route index element={<Registration />} />
-
+        <Route path="/registration" element={<Registration />} />
         <Route path="/login" element={<Login />} />
       </Route>
     )
