67b4b992935df0e3f9d2123c4ba3e36d60cafe11
Md. Shahriear Shuvo <shahriearshuvo@gmail.com>
2026-02-21 17:11:59 +0600

    conversation

--- src/Components/ConversationList.jsx
+++ src/Components/ConversationList.jsx
@@ -237,7 +237,7 @@ const ConversationList = ({ onBack }) => {
   };
 
   return (
-    <div className="flex-1 flex flex-col h-full bg-[url('/image/wp.jpg')] bg-cover bg-center">
+    <div className="flex-1 flex flex-col min-h-[100dvh] bg-[url('/image/wp.jpg')] bg-cover bg-center">
       {/* Header */}
      <div className="flex items-center justify-between border-b px-4 py-3 bg-white/70 backdrop-blur-md">
 
@@ -335,7 +335,7 @@ const ConversationList = ({ onBack }) => {
           type="submit"
           className="bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-blue-700 text-sm"
         >
-          Send
+          Sendd
         </button>
       </form>
     </div>
